3d79a308a1dba58c444028f4356c7ba3c7d5c5b8
edwardchairudin47977523 <edwardpandiya@gmail.com>
2025-08-18 23:51:41 +1000

    Creating Search Query field in model.py and migrate it

--- Backend/api/models.py
+++ Backend/api/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class project(models.Model):
+    Subject = models.CharField(max_length=100)
+    Location = models.CharField(max_length=100, blank= True)
+    industry = models.CharField(max_length=100, blank=True)
